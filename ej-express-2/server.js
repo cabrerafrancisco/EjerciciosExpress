b838f9e0535a8b9d5c12a31fdd7e283909a42340
franciCabrera <francabre97@gmail.com>
2021-05-10 16:16:52 -0300

    Agrego segundo ejercicio

---
+++ ej-express-2/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+ 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
+ 
+app.listen(3000)
